dc4fe88b002434fbd8a591d42c07b7aed812fc1c
loganbfries <logan.fries@uconn.edu>
2023-11-23 15:36:10 -0500

    adds iteration through games and gets points for and points against.

--- points_for_points_against.py
+++ points_for_points_against.py
@@ -30,3 +30,25 @@ def create_team_points_dict(teams):
 
 
 nfl_points_dict = create_team_points_dict(teams)
+
+# Iterate through each game and add the points for and points against data to the dictionary.
+
+for indx, game in completed_games.iterrows():
+    # Creates variables for the names of the listed team and their opponent.
+    team_name = game["tm_name"]
+    opponent_team_name = game["opp_name"]
+
+    # Creates variables for the listed teams score and their opponents scores.
+    team_score = game["tm_score"]
+    opponent_score = game["opp_score"]
+
+    # Adds the points for and points against data to the dictionary.
+    nfl_points_dict[team_name]["points_for"].append(team_score)
+    nfl_points_dict[team_name]["points_against"].append(opponent_score)
+
+    nfl_points_dict[opponent_team_name]["points_for"].append(opponent_score)
+    nfl_points_dict[opponent_team_name]["points_against"].append(team_score)
+
+for key in nfl_points_dict:
+    print(key, nfl_points_dict[key])
+    print()
